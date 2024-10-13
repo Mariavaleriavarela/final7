f3deed43fc354e38f343f853d983be5a34d27445
mariavaleriavarela <valeriamariaarevalovarela@gmail.com>
2024-10-13 20:32:15 -0300

    corregi importacion de prodcutos

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import styles from './ProductCard.module.css';
+import styles from './Productcard.module.css';
 
 const ProductCard = ({ product }) => {
   return (
